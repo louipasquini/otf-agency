110699673a3fa0c7855c0dba868409e8ae3dd11c
louipasquini <louipasquini@gmail.com>
2023-07-29 14:20:03 -0300

    feature: contact section

--- static/script.js
+++ static/script.js
@@ -3,14 +3,17 @@ const scroll = document.getElementById('scroll');
 
 function animeScroll() {
     const windowTop = window.pageYOffset;
+    console.log(windowTop)
     if (windowTop > 759 && windowTop < 1659) {
         scroll.style.marginTop = '20px'
     } else if (windowTop >= 1659 && windowTop < 2600) {
         scroll.style.marginTop = '35px'
-    } else if (windowTop > 2600) {
+    } else if (windowTop > 2600 && windowTop < 3500) {
         scroll.style.marginTop = '50px'
     } else if (windowTop <= 759) {
         scroll.style.marginTop = '0px'
+    } else if (windowTop > 3500) {
+        scroll.style.marginTop = '68px'
     }
 }
 
