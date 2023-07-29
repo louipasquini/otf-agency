194ae9a17291ce33259efe5d7462d604af3ecf07
louipasquini <louipasquini@gmail.com>
2023-07-29 13:05:25 -0300

    height and width settings

--- static/script.js
+++ static/script.js
@@ -3,14 +3,13 @@ const scroll = document.getElementById('scroll');
 
 function animeScroll() {
     const windowTop = window.pageYOffset;
-    console.log(windowTop)
-    if (windowTop > 900 && windowTop < 1800) {
+    if (windowTop > 759 && windowTop < 1659) {
         scroll.style.marginTop = '20px'
-    } else if (windowTop > 1800 && windowTop < 2700) {
+    } else if (windowTop >= 1659 && windowTop < 2600) {
         scroll.style.marginTop = '35px'
-    } else if (windowTop > 2700) {
+    } else if (windowTop > 2600) {
         scroll.style.marginTop = '50px'
-    } else if (windowTop < 900) {
+    } else if (windowTop <= 759) {
         scroll.style.marginTop = '0px'
     }
 }
